99d15c54a5863a6736239cf1eb879d59045d1d53
Tobias Azpiazu <tobiasazpia@gmail.com>
2024-01-24 21:58:35 -0300

    Boton basico

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import ItemCount from './components/ItemCount/ItemCount'
 
 const App = () => {
   return (
     <>
       <NavBar/>
       <ItemListContainer greeting="Primera entrega!"/>
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agreagada ", quantity)}/>
     </>
   )
 }
